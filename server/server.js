775ffbe269a5a0835a13ef8da1e9b0e45ff680dc
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-11-04 01:30:10 +0800

    M1: installed cors for simul testing of front-back
    only installed CORS in the backend, not sure if needed in the frontend

--- server/server.js
+++ server/server.js
@@ -3,11 +3,17 @@ const session = require('express-session');
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + "/"));
 
+app.use(cors({ 
+	credentials: true,
+	origin: true,
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+}));
 app.use(cookieParser());
 app.use(session({
 	secret: process.env.SECRET,
