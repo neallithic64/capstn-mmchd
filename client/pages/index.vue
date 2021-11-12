a0ea46262c8c72825b6f4b50c861cc8e9e3a78eb
julia <julia_estella@dlsu.edu.ph>
2021-11-12 09:50:16 +0800

    F1: removed authentication, not working properly

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -7,12 +7,12 @@
 
 <script>
 export default {
-  middleware({ store, redirect }) {
-      // If the user is not authenticated
-      if (!store.state.authenticated) {
-        return redirect('/login')
-      }
-  }
+  // middleware({ store, redirect }) {
+  //     // If the user is not authenticated
+  //     if (!store.state.authenticated) {
+  //       return redirect('/login')
+  //     }
+  // }
 }
 </script>
 
