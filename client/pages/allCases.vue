7928f3e9dd998d93a57c290a36c6cb5f67ae75c7
shannonho <shannon_gail_ho@dlsu.edu.ph>
2021-12-09 21:45:00 +0800

    M1: initial validation, popup eme fix, crf auth

--- client/pages/allCases.vue
+++ client/pages/allCases.vue
@@ -12,10 +12,14 @@
           <ul :class="formListClass('cif')" @click="clickTab('cif')">
             CIF
           </ul>
-          <ul :class="formListClass('crfDRU')" @click="clickTab('crfDRU')">
+          <ul v-if="$auth.user.userType === 'pidsrStaff' || $auth.user.userType === 'techStaff' ||
+            $auth.user.userType === 'BHS' || $auth.user.userType === 'RHU' || $auth.user.userType === 'CHO' ||
+            $auth.user.userType === 'govtHosp' || $auth.user.userType === 'privHosp' || $auth.user.userType === 'clinic' ||
+            $auth.user.userType === 'govtLab' || $auth.user.userType === 'privLab' || $auth.user.userType === 'airseaPort'"
+            :class="formListClass('crfDRU')" @click="clickTab('crfDRU')">
             CRF DRU
           </ul>
-          <ul :class="formListClass('crfCHD')" @click="clickTab('crfCHD')">
+          <ul v-if="$auth.user.userType === 'pidsrStaff' || $auth.user.userType === 'fhsisStaff' || $auth.user.userType === 'techStaff'">
             CRF CHD
           </ul>
         </div>
