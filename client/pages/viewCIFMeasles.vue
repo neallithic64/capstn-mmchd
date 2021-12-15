274ff1d4c09b00cacb673b4dae0f7b4018c20a0e
julia <julia_estella@dlsu.edu.ph>
2021-12-16 01:43:08 +0800

    M1: info tooltip CRF

--- client/pages/viewCIFMeasles.vue
+++ client/pages/viewCIFMeasles.vue
@@ -408,15 +408,7 @@
           <hr />
 
           <div id="case-investigation-form" class="center">
-            <div style="display: flex; flex-direction: row;">
-              <h2 id="form-header">Risk Factors</h2>
-              <button
-                class="tooltip"
-                data-tooltip="Identify all possible risk factors that may contribute to the analysis of disease spread"
-                data-tooltip-location="right">
-                <img class="tooltip-icon-img" src="~/assets/img/infoicon.png" />    
-              </button>
-            </div>
+            <h2 id="form-header">Risk Factors</h2>
             
             <div class="risk-flex">
               <div
@@ -977,24 +969,6 @@
               <div class="field">
                 <label for="complications">
                   Are there any complications? Please specify.
-                  <!-- <img
-                      id="infofever"
-                      class="info-icon-img"
-                      src="~/assets/img/infoicon.png"
-                    />
-                    <div class="info-desc infodesc-outside">
-                      {{ info.complications }}
-                    </div> -->
-                  <div class="tooltip shorttooltip">
-                    <span class="tooltipText shorttooltipText">{{
-                      info.complications
-                    }}</span>
-                    <img
-                      id="infofever"
-                      class="info-icon-img"
-                      src="~/assets/img/infoicon.png"
-                    />
-                  </div>
                 </label>
                 <input
                   id="complications"
@@ -1010,16 +984,6 @@
               <div class="field">
                 <label for="otherSymptoms">
                   Other symptoms
-                  <div class="tooltip shorttooltip">
-                    <span class="tooltipText shorttooltipText">{{
-                      info.otherSymptoms
-                    }}</span>
-                    <img
-                      id="infofever"
-                      class="info-icon-img"
-                      src="~/assets/img/infoicon.png"
-                    />
-                  </div>
                 </label>
                 <input
                   id="otherSymptoms"
@@ -1035,16 +999,6 @@
               <div class="field">
                 <label for="diagnosis">
                   Working/Final Diagnosis
-                  <div class="tooltip shorttooltip">
-                    <span class="tooltipText shorttooltipText">{{
-                      info.diagnosis
-                    }}</span>
-                    <img
-                      id="infofever"
-                      class="info-icon-img"
-                      src="~/assets/img/infoicon.png"
-                    />
-                  </div>
                 </label>
                 <input
                   id="diagnosis"
@@ -3104,195 +3058,6 @@ select {
   .vaccine-label {
     display: block;
   }
-}
-
-.tooltip-icon-img {
-  width: 10px;
-  height: 10px;
-  margin: 0 5px;
-  z-index: 1;
-}
-
-/* Positioning and visibility settings of the tooltip */
-[data-tooltip]:before,
-[data-tooltip]:after {
-  position: absolute;
-  visibility: hidden;
-  opacity: 0;
-  left: 50%;
-  bottom: calc(100% + 5px); /* 5px is the size of the arrow */
-  pointer-events: none;
-  transition: 0.2s;
-  will-change: transform;
-  font-family: 'Work Sans', sans-serif;
-}
-
-/* The actual tooltip with a dynamic width */
-[data-tooltip]:before {
-  content: attr(data-tooltip);
-  padding: 9px 10px;
-  min-width: 50px;
-  max-width: 300px;
-  width: max-content;
-  width: -moz-max-content;
-  border-radius: 6px;
-  font-size: 10px;
-  background-color: rgba(59, 72, 80, 0.9);
-  background-image: linear-gradient(30deg,
-    rgba(59, 72, 80, 0.44),
-    rgba(59, 68, 75, 0.44),
-    rgba(60, 82, 88, 0.44));
-  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.2);
-  color: #fff;
-  text-align: center;
-  white-space: pre-wrap;
-  transform: translate(-50%, -5px) scale(0.5);
-}
-
-/* Tooltip arrow */
-[data-tooltip]:after {
-  content: '';
-  border-style: solid;
-  border-width: 5px 5px 0px 5px; /* CSS triangle */
-  border-color: rgba(55, 64, 70, 0.9) transparent transparent transparent;
-  transition-duration: 0s; /* If the mouse leaves the element, 
-                              the transition effects for the 
-                              tooltip arrow are "turned off" */
-  transform-origin: top;   /* Orientation setting for the
-                              slide-down effect */
-  transform: translateX(-50%) scaleY(0);
-}
-
-/* Tooltip becomes visible at hover */
-[data-tooltip]:hover:before,
-[data-tooltip]:hover:after {
-  visibility: visible;
-  opacity: 1;
-}
-/* Scales from 0.5 to 1 -> grow effect */
-[data-tooltip]:hover:before {
-  transition-delay: 0.3s;
-  transform: translate(-50%, -5px) scale(1);
-}
-/* 
-  Arrow slide down effect only on mouseenter (NOT on mouseleave)
-*/
-[data-tooltip]:hover:after {
-  transition-delay: 0.5s; /* Starting after the grow effect */
-  transition-duration: 0.2s;
-  transform: translateX(-50%) scaleY(1);
-}
-/*
-  That's it for the basic tooltip.
-
-  If you want some adjustability
-  here are some orientation settings you can use:
-*/
-
-/* LEFT */
-/* Tooltip + arrow */
-[data-tooltip-location="left"]:before,
-[data-tooltip-location="left"]:after {
-  left: auto;
-  right: calc(100% + 5px);
-  bottom: 50%;
-}
-
-/* Tooltip */
-[data-tooltip-location="left"]:before {
-  transform: translate(-5px, 50%) scale(0.5);
-}
-[data-tooltip-location="left"]:hover:before {
-  transform: translate(-5px, 50%) scale(1);
-}
-
-/* Arrow */
-[data-tooltip-location="left"]:after {
-  border-width: 5px 0px 5px 5px;
-  border-color: transparent transparent transparent rgba(55, 64, 70, 0.9);
-  transform-origin: left;
-  transform: translateY(50%) scaleX(0);
-}
-[data-tooltip-location="left"]:hover:after {
-  transform: translateY(50%) scaleX(1);
-}
-
-
-
-/* RIGHT */
-[data-tooltip-location="right"]:before,
-[data-tooltip-location="right"]:after {
-  left: calc(100% + 5px);
-  bottom: 50%;
-}
-
-[data-tooltip-location="right"]:before {
-  transform: translate(5px, 50%) scale(0.5);
-}
-[data-tooltip-location="right"]:hover:before {
-  transform: translate(5px, 50%) scale(1);
-}
-
-[data-tooltip-location="right"]:after {
-  border-width: 5px 5px 5px 0px;
-  border-color: transparent rgba(55, 64, 70, 0.9) transparent transparent;
-  transform-origin: right;
-  transform: translateY(50%) scaleX(0);
-}
-[data-tooltip-location="right"]:hover:after {
-  transform: translateY(50%) scaleX(1);
-}
-
-/* BOTTOM */
-[data-tooltip-location="bottom"]:before,
-[data-tooltip-location="bottom"]:after {
-  top: calc(100% + 5px);
-  bottom: auto;
-}
-
-[data-tooltip-location="bottom"]:before {
-  transform: translate(-50%, 5px) scale(0.5);
-}
-[data-tooltip-location="bottom"]:hover:before {
-  transform: translate(-50%, 5px) scale(1);
-}
-
-[data-tooltip-location="bottom"]:after {
-  border-width: 0px 5px 5px 5px;
-  border-color: transparent transparent rgba(55, 64, 70, 0.9) transparent;
-  transform-origin: bottom;
-}
-
-.tooltip {
-  cursor: pointer;
-  text-align: center;
-  border: none;
-  border-radius: 4px;
-  outline: inherit;
-  text-decoration: none;
-  font-family: Roboto, sans-serif;
-  font-size: 0.7em;
-  /* background-color: rgba(174, 184, 192, 0.55); */
-  background-color: transparent;
-  color: white;
-
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  transition: background 350ms ease-in-out, 
-              transform 150ms ease;
-}
-.tooltip:hover {
-  /* background-color: #484f56; */
-  background-color: transparent;
-}
-.tooltip:active {
-  transform: scale(0.98);
-}
-.tooltip:focus {
-  box-shadow: 0 0 2px 2px #298bcf;
-}
-.tooltip::-moz-focus-inner {
-  border: 0;
 }
 
 label {
