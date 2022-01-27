818362688a4f3833a4fcd41c4ffffc4f00b1319b
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2022-01-27 19:26:28 +0800

    M4: getWeek() Date prototype method

--- server/controllers/m4Controller.js
+++ server/controllers/m4Controller.js
@@ -3,6 +3,14 @@ const saltRounds = 10;
 
 const db = require("../models/db");
 
+Date.prototype.getWeek = function() {
+	let d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+	let dayNum = d.getUTCDay() || 7;
+	d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+	let yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+	return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+}
+
 /** ON ID CREATION
 */
 function getPrefix(table) {
