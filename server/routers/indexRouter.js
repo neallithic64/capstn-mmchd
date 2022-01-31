a926776cae261c3415238457e36f16967eaf0df6
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2022-01-31 15:19:29 +0800

    M3: report bulletin, but no filtering

--- server/routers/indexRouter.js
+++ server/routers/indexRouter.js
@@ -44,6 +44,7 @@ router.get("/dashboardData", m2Cont.getIndexData);
 router.get("/getFileBlob", m3Cont.getFileBlob);
 router.get("/getReport", m3Cont.getReport);
 router.get("/getReports", m3Cont.getAllReports);
+router.get("/getReportBulletin", m3Cont.getReportBulletin);
 
 router.get("/getProgTargets", m4Cont.getAllProgTargets);
 router.get("/getProgAccomps", m4Cont.getAllProgAccomps);
