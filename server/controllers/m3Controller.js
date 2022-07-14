3363f8f8a965eef9633300eff563550fabdfd452
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2022-07-15 04:50:00 +0800

    M3: space

--- server/controllers/m3Controller.js
+++ server/controllers/m3Controller.js
@@ -40,7 +40,7 @@ function sendReportEmail(email, reportID, status) {
 		service: "gmail",
 		auth: {
 			type: "OAuth2",
-			user: process.env.EMAIL_ADDR, 
+			user: process.env.EMAIL_ADDR,
 			clientId: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
 			refreshToken: process.env.REFRESH_TOKEN,
